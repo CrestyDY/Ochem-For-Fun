df8da735278511e86e6a14c7507d7d081f4f9a61
CrestyDY <danielyumtl@hotmail.com>
2024-11-12 22:57:38 -0500

    Time_Trial updated

--- .idea/Ochem_Survival/Time_Trial.py
+++ .idea/Ochem_Survival/Time_Trial.py
@@ -14,6 +14,7 @@ class Time_Trial:
         self.start_time = None
         self.time_limit = 60  # 60 seconds for the time trial
         self.time_left = self.time_limit
+        pygame.font.init()
         self.font = pygame.font.SysFont('comicsansms', 36)
 
         # Background images
@@ -77,123 +78,54 @@ class Time_Trial:
             # Update the screen
             pygame.display.flip()
             self.clock.tick(30)  # 30 FPS for the time trial mode
+        pygame.quit()  # Clean up pygame resources when exiting the loop
 
-        def on_init(self):
-            pygame.init()
-            pygame.font.init()
+    def on_init(self):
+        pygame.init()
+        pygame.font.init()
 
-            # Display
-            self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        # Display
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
 
-            # Title and Icon
-            pygame.display.set_caption('Ochem Battle')
-            Icon = pygame.image.load('background.png')
-            pygame.display.set_icon(Icon)
+        # Title and Icon
+        pygame.display.set_caption('Ochem Battle')
+        Icon = pygame.image.load('background.png')
+        pygame.display.set_icon(Icon)
 
-            pygame.display.flip()
-            self._running = True
+        pygame.display.flip()
+        self._running = True
 
-        def toggle_background(self):
-            "Toggle between light and dark backgrounds."
-            if self.dark_mode:
-                self.current_background = self.background_light
-            else:
-                self.current_background = self.background_dark
-            self.dark_mode = not self.dark_mode
+    def toggle_background(self):
+        "Toggle between light and dark backgrounds."
+        if self.dark_mode:
+            self.current_background = self.background_light
+        else:
+            self.current_background = self.background_dark
+        self.dark_mode = not self.dark_mode
 
-        def on_event(self, event):
-            if event.type == pygame.QUIT:
-                self._running = False
+    def on_event(self, event):
+        pass
+    def on_loop(self):
+        pass
 
-            # Handle mouse clicks for button toggle
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.button_rect.collidepoint(event.pos):
-                    self.toggle_background()
+    def on_render(self):
+        # Update the display
+        pygame.display.flip()
 
-                # Handle gamemode button clicks
-                if self.gamemode1.is_clicked(event.pos):
-                    self.current_screen = "survival"  # Switch to survival screen
-                elif self.gamemode2.is_clicked(event.pos):
-                    self.current_screen = "time_trial"  # Switch to time trial screen
+    def on_cleanup(self):
+        pygame.quit()
 
-            # Handle mouse motion for hover effect
-            if event.type == pygame.MOUSEMOTION:
-                self.gamemode1.check_hover(event.pos)  # Check hover on gamemode1
-                self.gamemode2.check_hover(event.pos)  # Check hover on gamemode2
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
 
-        def on_loop(self):
-            pass
-
-        def on_render(self):
-            if self.current_screen == "playground":
-                # Set the background
-                self._display_surf.blit(self.current_background.image, self.current_background.rect)
-
-                # Draw the button with smooth corners (rounded rectangle)
-                button_color = (169, 169, 169) if self.dark_mode else (230, 230, 230)
-                pygame.draw.rect(self._display_surf, button_color, self.button_rect,
-                                 border_radius=10)  # Rounded corners
-
-                button_text = "Light" if self.dark_mode else "Lighter"
-                text_color = (255, 255, 255) if self.dark_mode else (0, 0, 0)
-
-                # Render the button text in white and position it in the center of the button
-                button_font = pygame.font.Font('freesansbold.ttf', 20)
-                button_text_surface = button_font.render(button_text, True, text_color)
-                button_text_rect = button_text_surface.get_rect(center=self.button_rect.center)
-                self._display_surf.blit(button_text_surface, button_text_rect)
-
-                # Set the transparent playground surface with rounded corners
-                playground_surface = pygame.Surface((self.playground_rect.width, self.playground_rect.height),
-                                                    pygame.SRCALPHA)
-                playground_color = (150, 150, 150, 180) if self.current_background == self.background_dark else (
-                169, 169, 169, 180)
-                pygame.draw.rect(playground_surface, playground_color, playground_surface.get_rect(), border_radius=50)
-                self._display_surf.blit(playground_surface, self.playground_rect)
-
-                # Game Title
-                title_text = "WELCOME TO OCHEM SURVIVAL !"
-                title_font = pygame.font.SysFont('comicsansms', 50)
-                title_surface = title_font.render(title_text, True, (0, 0, 0))
-                self._display_surf.blit(title_surface, (self.playground_rect.x + 275, self.playground_rect.y + 5))
-
-                # Subtitle
-                subtitle_text = "Are You Ready To Take On This Challenge ?"
-                subtitle_font = pygame.font.SysFont('comicsansms', 30)
-                subtitle_surface = subtitle_font.render(subtitle_text, True, (0, 0, 0))
-                self._display_surf.blit(subtitle_surface, (self.playground_rect.x + 375, self.playground_rect.y + 75))
-
-                # Sub-subtitle
-                subsubtitle_text = "Select a Gamemode"
-                subsubtitle_font = pygame.font.SysFont('comicsansms', 30)
-                subsubtitle_surface = subsubtitle_font.render(subsubtitle_text, True, (0, 0, 0))
-                self._display_surf.blit(subsubtitle_surface,
-                                        (self.playground_rect.x + 525, self.playground_rect.y + 125))
-
-                # Display Gamemode buttons
-                self.gamemode1.draw(self._display_surf)
-                self.gamemode2.draw(self._display_surf)
-
-            elif self.current_screen == "time_trial":
-                # If time trial mode is selected, run the Time_Trial class
-                time_trial = Time_Trial()
-                time_trial.run(self._display_surf)
-
-            # Update the display
-            pygame.display.flip()
-
-        def on_cleanup(self):
-            pygame.quit()
-
-        def on_execute(self):
-            if self.on_init() == False:
-                self._running = False
-
-            while self._running:
-                for event in pygame.event.get():
-                    self.on_event(event)
-                self.on_loop()
-                self.on_render()
-            self.on_cleanup()
-
+        while self._running:
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+if __name__ == "__main__":
+    theApp = Time_Trial()
+    theApp.on_execute()
 
