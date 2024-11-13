098e44c722d331b0f1e3c9a2f9b7cd39b891430d
CrestyDY <danielyumtl@hotmail.com>
2024-11-12 22:40:22 -0500

    Second commit

--- .idea/inspectionProfiles/formatted_database.py
+++
@@ -1,8 +0,0 @@
-import my_database
-
-temp_string = [str(i) for i in my_database.data_temperature]
-pKa_string = [str(i) for i in my_database.data_pKa]
-
-x = "Chemical Formula: {:141s} pKa: {:7s} Temperature: {:10s} Iupac Name: {:143s}"
-"""for i in range(len(my_database.final_data)):
-    print(x.format(my_database.data_formula[i], pKa_string[i], temp_string[i], my_database.data_iupac[i]))"""
